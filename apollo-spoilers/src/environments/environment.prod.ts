85c4556705292434b1b165a978f2de854c1457bc
Ramit <ramitkumaroffical@gmail.com>
2026-06-30 12:38:26 +0530

    .

--- apollo-spoilers/src/environments/environment.prod.ts
+++ apollo-spoilers/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.apollospoilers.com/api/v1',
+  apiUrl: 'https://api.apollospoilers.com/api/v1.0',
   imageUrl: 'https://api.apollospoilers.com'
 };
